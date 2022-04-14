99a3b2443c615a7406b6259be028eebb9a9094de
Marco Munizaga <git@marcopolo.io>
2022-04-14 12:16:22 -0700

    Create slides

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "A very basic flake";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { system = system; };
+      in
+      {
+        packages.hello = pkgs.hello;
+        defaultPackage = self.packages.${system}.hello;
+        devShell = pkgs.mkShell {
+          buildInputs = [ pkgs.jq ];
+        };
+      });
+}
